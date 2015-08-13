6031653583f69924b1aa4ffe7c8515346f3b64f8
unknown <reggie.senal@ReggieS-14030.ddb.sing>
2015-08-13 11:59:30 +0800

    first commit for CQ web app

---
+++ cqwebapp/bundle/src/test/java/com/samplecorp/cqwebapp/SimpleUnitTest.java
@@ -0,0 +1,14 @@
+package com.samplecorp.cqwebapp;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleUnitTest {
+
+    @Test
+    public void someTest() {
+        assertTrue(true);
+    }
+
+}
